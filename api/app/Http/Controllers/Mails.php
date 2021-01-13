6a610c0b11478ab5c8197c0e0175b3cc2f5aad70
valay <angelvalay@gmail.com>
2021-01-12 22:48:29 -0600

    creacion del controlador y rutas

---
+++ api/app/Http/Controllers/Mails.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class Mails extends Controller
+{
+    public function store(Response $response)
+    {
+        // validate
+        // create instance
+        // return instance
+    }
+
+    public function show($id)
+    {
+        // find instance
+        // return instance
+    }
+
+    public function mark(Response $response)
+    {
+        // validate
+        // update instances
+        // return exit o fail
+    }
+
+    public function trash()
+    {
+        // get instances
+        // return instances
+    }
+
+    public function index()
+    {
+        // get instances
+        // return instances
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -5,5 +5,36 @@
 use Laravel\Lumen\Routing\Router;
 
 $router->get('/', function () use ($router) {
-    return '$router->app->version()';
+    return redirect('/mails');
+});
+
+$router->group(['prefix' => 'mails'], function () use ($router) {
+    $router->get('/', function () {
+        return 'return mails sent';
+    });
+
+    $router->get('/{id}', function ($id) {
+        return 'return content mail '. $id;
+    });
+
+    $router->get('/deleted', function () {
+        return 'return mails trashed';
+    });
+
+    $router->put('/mark', function (){
+        // id's array, true o false for mark as important
+        return 'mark as important a set mails';
+    });
+
+    $router->patch('/{id}', function ($id){
+        return 'send mail to trash '.$id;
+    });
+
+    $router->delete('/{id}', function ($id) {
+        return 'delete definitely mail '.$id;
+    });
+
+    $router->post('/', function () {
+        return 'send info mail';
+    });
 });
