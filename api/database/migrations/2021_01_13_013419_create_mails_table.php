8b4cb06ddd35c38d5a919d1dbd40d397eb761d6b
valay <angelvalay@gmail.com>
2021-01-12 21:57:42 -0600

    creacion del modelo, migracion

---
+++ api/database/migrations/2021_01_13_013419_create_mails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('mail_from');
+            $table->string('mail_to');
+            $table->text('body');
+            $table->boolean('is_important')->default(false);
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mails');
+    }
+}
